89a85eeba42b62a2510d58a3a7e8fd5af8b8c520
Deven Patel <deven.h.j.patel+softwaredev@gmail.com>
2023-10-11 12:02:25 -0400

    AM algo

---
+++ DLL.js
@@ -0,0 +1,143 @@
+/* 
+TODO: Create the DLLNode class and implement the DoublyLinkedList constructor
+and the empty methods below the constructor.
+*/
+class DLLNode {
+    constructor(data) {
+        this.data = data;
+        this.prev = null;
+        this.next = null;
+    }
+}
+/**
+ * A class to represent a doubly linked list and contain all of it's methods.
+ * A doubly linked list is a singly linked list that can be traversed in both
+ * directions.
+ */
+class DoublyLinkedList {
+    /**
+     * Executed when the new keyword is used to construct a new DoublyLInkedList
+     * instance that inherits these methods and properties.
+     */
+    // constructor
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    /**
+     * Creates a new node and adds it at the front of this list.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {any} data The data for the new node.
+     * @returns {DoublyLinkedList} This list.
+     */
+    insertAtFront(value) {
+        const newNode = new DLLNode(data);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            newNode.next = this.head;
+            this.head.prev = newNode;
+            this.head = newNode;
+        }
+        return this;
+    }
+
+    /**
+     * Creates a new node and adds it at the back of this list.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {any} data The data for the new node.
+     * @returns {DoublyLinkedList} This list.
+     */
+    insertAtBack(data) {
+        const newNode = new DLLNode(data);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            newNode.prev = this.tail;
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        return this;
+    }
+    // EXTRA
+    /**
+     * Removes the middle node in this list.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @returns {any} The data of the removed node.
+     */
+    removeMiddleNode() {
+        if (!this.head) {
+            return null;
+        }
+        let length = 0;
+        let current = this.head;
+
+        while (current) {
+            length++;
+            current = current.next;
+        }
+
+        let midPoint = Math.floor(length / 2);
+        current = this.head;
+        let position = 0;
+        while (position < midPoint) {
+            current = current.next;
+            position++;
+        }
+        let lnode = current.prev;
+        let rnode = current.next;
+
+        lnode.next = rnode;
+        rnode.prev = lnode;
+
+        return current.data;
+    }
+
+    /**
+     * Determines if this list is empty.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @returns {boolean} Indicates if this list is empty.
+     */
+    isEmpty() {}
+
+    /**
+     * Converts this list to an array of the node's data.
+     * - Time: O(n) linear, n = list length.
+     * - Space: O(n) linear, array grows as list length increases.
+     * @returns {Array<any>} All the data of the nodes.
+     */
+    toArray() {
+        const vals = [];
+        let runner = this.head;
+
+        while (runner) {
+            vals.push(runner.data);
+            runner = runner.next;
+        }
+        return vals;
+    }
+
+    /**
+     * Adds all the given items to the back of this list.
+     * @param {Array<any>} items Items to be added to the back of this list.
+     * @returns {DoublyLinkedList} This list.
+     */
+    insertAtBackMany(items = []) {
+        items.forEach((item) => this.insertAtBack(item));
+        return this;
+    }
+}
+
+const emptyList = new DoublyLinkedList();
+const list1 = new DoublyLinkedList();
+list1.insertAtBackMany([5, 8, 13, 14]);
+console.log(list1.toArray());
+console.log(list1.removeMiddleNode());
+console.log(list1.toArray());
